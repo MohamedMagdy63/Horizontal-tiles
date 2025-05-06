6d30a3a72fb31cfa5d33ef46d0ed84cffee25711
MohamedMagdy63 <mohammedmagdy6522@gmail.com>
2025-05-06 19:46:55 +0300

    Add Logo and the title

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { Suspense } from 'react'
-import { Logo } from '@pmndrs/branding'
 import './styles.css'
 import { App } from './App'
 
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
-        <br />
-        dev collective
-      </a>
+      <p style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        WebX-Crafters
+      </p>
       <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>bad —</div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>10/17/2021</div>
     </div>
@@ -24,6 +21,5 @@ createRoot(document.getElementById('root')).render(
       <App />
     </Suspense>
     <Overlay />
-    <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
   </>
 )
